03ba30dd301da32a183f28ab2876dbe6e0f33499
Mateus <mateuaraujo01@gmail.com>
2025-10-08 19:10:40 -0300

    act 9

--- act2/main9.py
+++ act2/main9.py
@@ -11,6 +11,31 @@ class Rectangle:
     def height(self):
         return f"{self._height: .1f}cm"
 
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+
+    @width.deleter
+    def width(self):
+        del self._width
+        print("width has been deleted")
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("height has been deleted")
 
 rectangle = Rectangle(3, 4)
+rectangle.height = 0 #cannot be less than 0
+rectangle.width = 5
 print(rectangle.height, rectangle.width)
+
+del rectangle.width
+del rectangle.height
+#print(rectangle.width) se printar isso retorna em erro porque width em rectangle ja foi deletado (nesse rectangle)
